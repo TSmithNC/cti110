648bbccda94e283742f76369e82a1c00d93b69e2
TSmithNC <160090695+TSmithNC@users.noreply.github.com>
2024-02-14 22:49:50 -0500

    Add files via upload
    Upload for P1LAB1 assignment.

---
+++ P1LAB1_SmithTyler.py
@@ -0,0 +1,10 @@
+
+#Tyler Smith
+#2/13/2024
+#P1LAB1
+#here we will create use input to set a variable for First Name
+#the prompt will first ask for your name
+#after inputting a name, will say "welcome to zybooks"
+
+firstname = input('Hello! What is your name?')
+print('Hey, ' + firstname+'!\nWelcome to ZyBooks!')
